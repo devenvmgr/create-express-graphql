c6ef31098a5b9478471693ee546e54c6857ad54f
Jason <jason@casjay.net>
2021-10-27 15:25:07 -0400

    🗃 Modified: LICENSE.md package-lock.json package.json server/index.js  🗃

--- server/index.js
+++ server/index.js
@@ -31,4 +31,4 @@ app.use(
 );
 
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running GraphQL API server at http://localhost:4000/graphql');
